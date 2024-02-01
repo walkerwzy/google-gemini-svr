496c11e4992a7d1dc1ca336484a0ee4e9aa5109d
walker <e-zhongying.wang@lotuscars.com.cn>
2024-02-01 18:42:29 +0800

    添加网页

--- server/server.js
+++ server/server.js
@@ -1,20 +1,28 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from "path"
 
 import { appConfig } from "./config/appConfig.js";
 import { aiController } from "./controllers/aiController.js";
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
-app.use(
-  cors({
-    origin: appConfig.corsConfig.origin,
-    methods: appConfig.corsConfig.methods,
-    allowedHeaders: ["Content-Type", "application/json"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: appConfig.corsConfig.origin,
+//     methods: appConfig.corsConfig.methods,
+//     allowedHeaders: ["Content-Type", "application/json"],
+//   })
+// )
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 3000;
 
 // Get Gemini API Response
